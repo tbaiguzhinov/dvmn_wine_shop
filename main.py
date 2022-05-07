6b3591c0b592681cd29142baa990eaddb8fedb70
Baiguzhinov <MOSBAIGUT@CORP.JTI.COM>
2022-05-07 16:33:09 +0600

    Rewrote README, added example file, removed unnecessary files, fixed main.py: reordered imports, added a functionality to indicate the location of file instead of only its name

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
-import datetime
-import pandas as pd
-from collections import defaultdict
+import argparse
 
+from collections import defaultdict
+from datetime import date
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+import pandas as pd
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def create_index():
+    FOUNDATION_YEAR = 1920
+    
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-current_age = str(datetime.date.today().year - 1920)
-if current_age.endswith("1") and not current_age.endswith("11"):
-    year_in_russian = "год"
-elif (current_age.endswith("2") and not current_age.endswith("12") or
-      current_age.endswith("3") and not current_age.endswith("13") or
-      current_age.endswith("4") and not current_age.endswith("14")):
-    year_in_russian = "года"
-else:
-    year_in_russian = "лет"
+    template = env.get_template('template.html')
 
-drinks = pd.read_excel("wine3.xlsx", keep_default_na=True).fillna("").to_dict(orient="records")
+    current_age = str(date.today().year - FOUNDATION_YEAR)
+    if current_age.endswith("1") and not current_age.endswith("11"):
+        year_in_russian = "год"
+    elif (current_age.endswith("2") and not current_age.endswith("12") or
+          current_age.endswith("3") and not current_age.endswith("13") or
+          current_age.endswith("4") and not current_age.endswith("14")):
+        year_in_russian = "года"
+    else:
+        year_in_russian = "лет"
 
-new_drinks = defaultdict(list)
+    parser = argparse.ArgumentParser(
+        description="Программа обновляет index.html из данных, полученных из файла"
+    )
+    parser.add_argument("filepath", help="Полный адрес файла")
+    args = parser.parse_args()
 
-for drink in drinks:
-    new_drinks[drink["Категория"]].append(drink)
+    drinks = pd.read_excel(args.filepath, keep_default_na=True).fillna("").to_dict(orient="records")
 
-rendered_page = template.render(
-    age = current_age,
-    year = year_in_russian,
-    drinks = new_drinks
-)
+    drinks_sorted_by_category = defaultdict(list)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    for drink in drinks:
+        drinks_sorted_by_category[drink["Категория"]].append(drink)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    rendered_page = template.render(
+        age = current_age,
+        year = year_in_russian,
+        all_drinks = drinks_sorted_by_category
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+def main():
+    create_index()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    main()
+    
