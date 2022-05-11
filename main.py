b9fe08ff502dfd681fe20d51b35aa61903fe4c2a
Baiguzhinov <MOSBAIGUT@CORP.JTI.COM>
2022-05-11 14:15:26 +0600

    Fixed identation in template, fixed style in main.py, rewrote function names

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-
 from collections import defaultdict
 from datetime import date
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -7,49 +6,48 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-def year_name(current_age):
-    if current_age%10 == 1 and not current_age%100 == 11:
+
+def identify_year_name(current_age):
+    if current_age % 10 == 1 and not current_age % 100 == 11:
         return "год"
-    elif (current_age%10 == 2 and not current_age%100 == 12 or
-          current_age%10 == 3 and not current_age%100 == 13 or
-          current_age%10 == 4 and not current_age%100 == 14):
+    elif (current_age % 10 == 2 and not current_age % 100 == 12 or
+          current_age % 10 == 3 and not current_age % 100 == 13 or
+          current_age % 10 == 4 and not current_age % 100 == 14):
         return "года"
     else:
         return "лет"
 
+
 def get_drinks(filepath):
-    drinks = pd.read_excel(filepath, keep_default_na=True).fillna("").to_dict(orient="records")
+    drinks = pd.read_excel(filepath).fillna("").to_dict(orient="records")
     drinks_sorted_by_category = defaultdict(list)
     for drink in drinks:
         drinks_sorted_by_category[drink["Категория"]].append(drink)
     return drinks_sorted_by_category
 
+
 def create_index():
     foundation_year = 1920
-    
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-
     template = env.get_template('template.html')
-
     current_age = date.today().year - foundation_year
-
     parser = argparse.ArgumentParser(
-        description="Программа обновляет index.html из данных, полученных из файла"
+        description="Программа обновляет index.html из данных файла"
     )
-    parser.add_argument("filepath", help="Полный адрес файла")
+    parser.add_argument("--filepath", help="Полный адрес файла",
+                        default="wine.xlsx")
     args = parser.parse_args()
-
     rendered_page = template.render(
-        age = current_age,
-        declension = year_name(current_age),
-        all_drinks = get_drinks(args.filepath),
+        age=current_age,
+        declension=identify_year_name(current_age),
+        all_drinks=get_drinks(args.filepath),
     )
-
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
+
 
 def main():
     create_index()
@@ -58,4 +56,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
